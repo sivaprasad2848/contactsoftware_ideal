9afce6c194b0e0d4ff08bcb1908b925a385e2aac
sivaprasad2848 <sivaprasad@cybersquare.org>
2026-04-02 14:52:56 +0530

    Delete and Update Added

--- index.py
+++ index.py
@@ -4,6 +4,8 @@ while y==0:
     print("Menu")
     print("1->For insert new contact")
     print("2->For Display contact")
+    print("3->For Delete Contact")
+    print("4->For Update")
     opt=int(input("Enter Your Option"))
     if(opt==1):
         name=input("Enter Name")
@@ -12,5 +14,14 @@ while y==0:
         contact.append((name,email,mobile))
     if(opt==2):
         print(contact)
+    if(opt==3):
+        i=int(input("Enter the index you want to remove"))
+        contact.pop(i)
+    if(opt==4):
+        i=int(input("Enter the index you want to update"))
+        name=input("Enter Name")
+        email=input("Enter Email")
+        mobile=input("Enter Mobile")
+        contact[i]=(name,email,mobile)
     y=int(input("Do you want to continue?Press 0 for yes"))
 
